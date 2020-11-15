4e19918288b2201786e08afdc0aa5bea1ae7f11d
Carl Rizk <carl.rizk@gotocme.com>
2020-11-15 12:53:52 +0200

    Revamp Navbar Add Font Awesome to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,32 @@ const sass = require('gulp-sass')
 sass.compiler = require('node-sass')
 const del = require('del')
 const browserSync = require('browser-sync')
+const concat = require('gulp-concat')
+//const sourcemMaps = require('gulp-sourcemaps')
+//const cssNano = require('gulp-cssnano')
 
 const config = {
     styles: {
         source: 'src/sass/**/*.scss',
-        destination: 'dist/css/'
+        destination: 'dist/css/',
     },
     templates: {
         source: 'src/**/*.html',
         destination: 'dist/'
     },
-    images: {
-        source: 'assets/img/**/*.{png,jpeg}',
-        destination: 'dist/assets/img/'
-    }
+    assets: {
+        source: 'assets/**/*.*',
+        destination: 'dist/assets/',
+        libraries: ['node_modules/@fortawesome/fontawesome-free/webfonts*/*']
+    },
+    js: {
+        source: 'src/js/**/*.js',
+        destination: 'dist/js/',
+        libraries: [
+            'node_modules/jquery/dist/jquery.slim.js',
+            'node_modules/bootstrap/dist/js/bootstrap.bundle.js'
+        ]
+    },
 }
 
 const reloadBrowser = function (done) {
@@ -25,7 +37,7 @@ const reloadBrowser = function (done) {
 };
 
 const watchSource = function (done) {
-    gulp.watch('src/', gulp.series(exports.default, reloadBrowser));
+    gulp.watch('src/', gulp.series(exports.buildSrc, reloadBrowser));
     done();
 };
 
@@ -43,13 +55,16 @@ const startServer = function (done) {
 exports.buildStyles = function (done) {
     gulp.src(config.styles.source)
         .pipe(sass().on('error', sass.logError))
+        //.pipe(sourcemMaps.init())
+        //.pipe(cssNano())
+        //.pipe(sourcemMaps.write())
         .pipe(gulp.dest(config.styles.destination));
     done();
 };
 
-exports.buildImages = function (done) {
-    gulp.src(config.images.source)
-        .pipe(gulp.dest(config.images.destination));
+exports.buildAssets = function (done) {
+    gulp.src([config.assets.source].concat(config.assets.libraries))
+        .pipe(gulp.dest(config.assets.destination));
     done()
 }
 
@@ -59,19 +74,33 @@ exports.buildTemplates = function (done) {
     done()
 }
 
+exports.buildScripts = function (done) {
+    gulp.src([config.js.source].concat(config.js.libraries))
+        //.pipe(sourcemMaps.init())
+        .pipe(concat('main.js'))
+        //.pipe(sourcemMaps.write())
+        .pipe(gulp.dest(config.js.destination));
+    done()
+}
+
 exports.clean = function (done) {
     del.sync(['dist/']);
 
     return done();
 };
 
-exports.default = gulp.series(
-    exports.clean,
+exports.buildSrc = gulp.series(
     gulp.parallel(
         exports.buildStyles,
-        exports.buildImages,
-        exports.buildTemplates
+        exports.buildTemplates,
+        exports.buildScripts
     )
+)
+
+exports.default = gulp.series(
+    exports.clean,
+    exports.buildSrc,
+    exports.buildAssets
 );
 
 exports.watch = gulp.series(
