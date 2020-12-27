4e72330d72c2030ade60ffa47ed413318db5473d
Karine <karine.ismail20@gmail.com>
2020-12-27 22:53:56 +0200

    Frontend Updates

--- frontend/src/app/components/request-detail-panel/request-detail-panel.component.ts
+++ frontend/src/app/components/request-detail-panel/request-detail-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {Request, Needs, Area, Gender} from '../../common/models/request';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-request-detail-panel',
@@ -10,9 +11,27 @@ export class RequestDetailPanelComponent implements OnInit {
   
   @Input() requests: Request[] = [Request.Nil];
 
-  constructor() { }
+  public name: string = 'Bookmark';
+  public title: string = 'Not Bookmarked';
+ 
+
+  constructor(private location: Location) { }
 
   ngOnInit(): void {
+  }
+
+  setfunction(): void {
+    if(this.name === 'Bookmark') { 
+      this.name = 'Remove from Bookmarks'
+      this.title = 'Bookmarked'
+    } else {
+      this.name = 'Bookmark'
+      this.title = 'Not Bookmarked'
+    }
+  }
+
+  goBack(): void {
+    this.location.go("requests");
   }
 
 }
